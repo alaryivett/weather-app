6cda3202e0dc32ff6ccfcfb12e47fd79f41e3992
alaryivett <ann-bo315@yandex.ru>
2022-12-04 16:06:47 +0400

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.wrapper
+	header
+		Icon(
+			iconName='logo'
+		)
+		h1 {{ constants.messages.title }}
+	nuxt
+</template>
+
+<script>
+import constants from '~/constants/layouts/default'
+
+export default {
+	data () {
+		return {
+			constants
+		}
+	}
+}
+</script>
+
+<style lang="sass">
+.wrapper
+	width: 100%
+	height: 100vh
+	background-color: #161B30
+	header
+		background-color: #2A2F45
+		padding: 17px 23px
+		display: flex
+		align-items: center
+		gap: 12px
+		h1
+			font-size: 16px
+			font-family: 'SFPro700'
+			color: white
+			margin: 0
+</style>
